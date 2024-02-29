e2773cb40fa8653e8977f8dc6c9fc2f21490d5af
itsmeneha007 <nehabaghel11092003@gmail.com>
2024-02-29 13:34:29 +0530

    wrightcode2

---
+++ wrightcode2.js
@@ -0,0 +1,13 @@
+let a = 5  ,  b = 0
+for(var  i=1; i<=5; i++){
+    if(a%i==0){
+        b++
+    }
+}
+if (b==2){
+    console.log("muber is prime" )
+
+}
+else {
+    console.log("number is not prime")
+}
